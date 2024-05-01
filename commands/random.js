1a96a8c14ca388f1f215b2c6eb63f995b686405c
VEDA00133912 <143770907+VEDA00133912@users.noreply.github.com>
2024-05-01 13:20:45 +0900

    Create random.js
    ランダム文字列を生成するrandom.jsを追加

---
+++ commands/random.js
@@ -0,0 +1,32 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('random')
+    .setDescription('ランダム文字生成')
+    .addIntegerOption(option =>
+      option.setName('length')
+        .setDescription('生成したい文字数')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const length = interaction.options.getInteger('length');
+
+    if (length > 1999) {
+      try {
+        await interaction.reply("2000以下にしてください");
+      } catch (error) {
+        console.error(error);
+        console.log("random error");
+      }
+    } else {
+      const randomString = Array.from({ length }, () => Math.random().toString(36).charAt(2)).join("");
+      try {
+        await interaction.reply(randomString);
+      } catch (error) {
+        console.error(error);
+        console.log("randomcreate error");
+      }
+    }
+  },
+};
