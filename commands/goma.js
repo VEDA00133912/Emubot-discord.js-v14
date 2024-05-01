1535b04e4e04bd74703f436051eb9604ae0a1af4
VEDA00133912 <143770907+VEDA00133912@users.noreply.github.com>
2024-05-01 13:17:15 +0900

    Create goma.js
    ゴママヨ動画を送信するgoma.jsを追加

---
+++ commands/goma.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('goma')
+    .setDescription('ゴママヨを送信します。'),
+  async execute(interaction) {
+    try {
+      await interaction.reply("[ごままよ](https://www.youtube.com/watch?v=NofBgAsZjxI)");
+    } catch (error) {
+      console.error(error);
+      console.log("goma error");
+    }
+  },
+};
