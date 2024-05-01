5160597f8e056d314404e9b901b923f47abe19e2
VEDA00133912 <143770907+VEDA00133912@users.noreply.github.com>
2024-05-01 13:19:18 +0900

    Create ping.js
    ping表示するためのping.jsを追加

---
+++ commands/ping.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('ping'),
+  async execute(interaction) {
+    const embed = new EmbedBuilder()
+      .setColor(0xf8b4cb)
+      .setTitle('bot｜ping')
+      .setDescription('Ping値')
+      .addFields(
+        { name: 'WebSocket Ping', value: `${interaction.client.ws.ping}ms`, inline: true },
+        { name: 'API-Endpoint Ping', value: `${Date.now() - interaction.createdTimestamp}ms`, inline: true },
+        );
+
+    try {
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error(error);
+      console.log("serverinfo error");
+    }
+  },
+};
