e8c59668dd7addcf9aa42554a4ff2d073bd6928b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-03 09:24:35 +0900

    Add files via upload

---
+++ commands/prime.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('prime')
+        .setDescription('素数判定')
+        .addIntegerOption(option =>
+            option.setName('number')
+                .setDescription('素数かどうかを判定したい数')
+                .setRequired(true)),
+    async execute(interaction) {
+        const number = interaction.options.getInteger('number');
+        let isPrime = true;
+
+        if (number <= 1) {
+            isPrime = false;
+        } else {
+            for (let i = 2; i <= Math.sqrt(number); i++) {
+                if (number % i === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        const Embed = new EmbedBuilder()
+            .setColor(0xf8b4cb)
+            .setTitle('素数判定結果')
+          .addFields(
+            { name: '入力された数', value: number.toString()},
+            { name: '結果', value: isPrime ? '素数だよ' : '素数じゃないよ'},
+            );
+
+        await interaction.reply({ embeds: [Embed] });
+    },
+};
