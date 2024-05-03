ec9ceea2ce2c498807be86a6f0c32997e3f8037e
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-03 16:00:58 +0900

    Update server.js
    Embed化

--- commands/server.js
+++ commands/server.js
@@ -1,14 +1,20 @@
 // 参加しているサーバー一覧を表示するコマンド
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('server')
     .setDescription('入ってる鯖一覧'),
   async execute(interaction) {
-    const serverInfo = interaction.client.guilds.cache.map(guild => `${guild.name}`).join("\n");
+    const guilds = interaction.client.guilds.cache;
+      
+    const embed = new EmbedBuilder()
+      .setColor(0xf8b4cb)
+      .setTitle('サーバー一覧')
+      .setDescription(guilds.map(guild => guild.name).join('\n'));
+
     try {
-      await interaction.reply(`\`\`\`\n${serverInfo}\n\`\`\``);
+      await interaction.reply({ embeds: [embed] });
     } catch (error) {
       console.error(error);
       console.log("server error");
