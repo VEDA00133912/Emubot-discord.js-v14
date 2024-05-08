6c6d69e490e0b0407c3174353c17713d2ea5b7bb
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-08 14:47:59 +0900

    Update 1000choyen.js
    optionで文字列取得するように変更

--- commands/1000choyen.js
+++ commands/1000choyen.js
@@ -1,41 +1,27 @@
-// https://qiita.com/narikakun/items/74e24c92709d7364ef9e様のをパクってます(((((
-const { SlashCommandBuilder, ModalBuilder, TextInputBuilder, ActionRowBuilder, TextInputStyle, EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('1000choyen')
-		.setDescription('1000兆円画像生成'),
+		.setName('5000choyen')
+		.setDescription('5000兆円欲しい!!風の画像生成')
+		.addStringOption(option =>
+			option.setName('top')
+				.setDescription('上部文字列')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('bottom')
+				.setDescription('下部文字列')
+				.setRequired(true),
+		),
 	async execute(interaction) {
-		const modal = new ModalBuilder()
-			.setCustomId('1000choyen')
-			.setTitle('1000兆円画像生成');
+		const top = interaction.options.getString('top');
+		const bottom = interaction.options.getString('bottom');
 
-		const topInput = new TextInputBuilder()
-			.setCustomId('topInput')
-			.setLabel("上部文字列")
-			.setStyle(TextInputStyle.Short);
+		const embed = new EmbedBuilder()
+			.setColor(0xf8b4cb)
+			.setImage(`https://gsapi.cbrx.io/image?top=${encodeURIComponent(top)}&bottom=${encodeURIComponent(bottom)}&type=png`);
 
-		const bottomInput = new TextInputBuilder()
-			.setCustomId('bottomInput')
-			.setLabel("下部文字列")
-			.setStyle(TextInputStyle.Short);
-
-		modal.addComponents(new ActionRowBuilder().addComponents(topInput), new ActionRowBuilder().addComponents(bottomInput));
-
-		await interaction.showModal(modal);
-		const filter = (mInteraction) => mInteraction.customId === '1000choyen';
-		interaction.awaitModalSubmit({ filter, time: 360000 })
-			.then(async mInteraction => {
-				const top = mInteraction.fields.getTextInputValue('topInput');
-				const bottom = mInteraction.fields.getTextInputValue('bottomInput');
-
-				const embed = new EmbedBuilder()
-					.setColor(0xf8b4cb)
-					.setImage(`https://gsapi.cbrx.io/image?top=${encodeURIComponent(top)}&bottom=${encodeURIComponent(bottom)}&type=png`);
-
-				mInteraction.reply({ embeds: [embed] });
-			})
-			.catch(console.error);
+		await interaction.reply({ embeds: [embed] });
 	},
 };
-
