fffae668cae3ddbe667dc1d5ef016be3b116a801
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-23 17:34:55 +0900

    Update prime.js

--- commands/prime.js
+++ commands/prime.js
@@ -1,37 +1,41 @@
-// 素数判定をするコマンド
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('prime')
-        .setDescription('素数判定')
-        .addIntegerOption(option =>
-            option.setName('number')
-                .setDescription('素数かどうかを判定したい数')
-                .setRequired(true)),
-    async execute(interaction) {
-        const number = interaction.options.getInteger('number');
-        let isPrime = true;
+  data: new SlashCommandBuilder()
+    .setName('prime')
+    .setDescription('素数判定')
+    .addIntegerOption(option =>
+      option.setName('number')
+        .setDescription('素数かどうかを判定したい数')
+        .setRequired(true)),
+  async execute(interaction) {
+    try {
+      const number = interaction.options.getInteger('number');
+      let isPrime = true;
 
-        if (number <= 1) {
+      if (number <= 1) {
+        isPrime = false;
+      } else {
+        for (let i = 2; i <= Math.sqrt(number); i++) {
+          if (number % i === 0) {
             isPrime = false;
-        } else {
-            for (let i = 2; i <= Math.sqrt(number); i++) {
-                if (number % i === 0) {
-                    isPrime = false;
-                    break;
-                }
-            }
+            break;
+          }
         }
+      }
 
-        const Embed = new EmbedBuilder()
-            .setColor(0xf8b4cb)
-            .setTitle('素数判定結果')
-          .addFields(
-            { name: '入力された数', value: number.toString()},
-            { name: '結果', value: isPrime ? '素数です' : '素数ではないです'},
-            );
+      const embed = new EmbedBuilder()
+        .setColor(0xf8b4cb)
+        .setTitle('素数判定結果')
+        .addFields(
+          { name: '入力された数', value: number.toString() },
+          { name: '結果', value: isPrime ? '素数です' : '素数ではないです' },
+        );
 
-        await interaction.reply({ embeds: [Embed] });
-    },
+      await interaction.reply({ embeds: [embed] });
+    } catch (error) {
+      console.error('素数判定中にエラーが発生しました：', error);
+      await interaction.reply('素数判定中にエラーが発生しました。');
+    }
+  },
 };
