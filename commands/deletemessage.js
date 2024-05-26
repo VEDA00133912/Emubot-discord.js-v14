275b37c66cba5827eedb4b3c252d5a3ff25e5c68
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-26 19:16:08 +0900

    Update deletemessage.js

--- commands/deletemessage.js
+++ commands/deletemessage.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder,EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -9,30 +9,31 @@ module.exports = {
       .setDescription('削除したいメッセージの数')
       .setRequired(true)),
 
-      async execute(interaction) {
-         if (!interaction.member.permissions.has("MANAGE_MESSAGES")) {
-            return interaction.reply({ content: 'このコマンドを実行する権限がありません。', ephemeral: true });
-        }
-        try {
-            await interaction.deferReply({ ephemeral: true });
+async execute(interaction) {
+  if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+    return interaction.reply({ content: 'このコマンドを実行する権限がありません。', ephemeral: true });
+  }
 
-          const count = interaction.options.getInteger('count');
-      
-          const messages = await interaction.channel.messages.fetch({ limit: count });
-          const promises = messages.map(msg => msg.delete());
-          await Promise.all(promises);
-      
-          const embed = new EmbedBuilder()
-            .setColor(0xf8b4cb)
-            .setTitle('削除完了！')
-            .setDescription(`削除したメッセージ数: ${promises.length}`);
+  try {
+    await interaction.deferReply({ ephemeral: true });
 
-          await interaction.editReply({ content: '削除完了！' });
-          interaction.channel.send({ embeds: [embed] });
-        } catch (error) {
-          console.error('メッセージの削除中にエラーが発生しました:', error);
-          interaction.channel.send('メッセージの削除中にエラーが発生しました');
-        }
-      }
-      ,
+    const count = interaction.options.getInteger('count');
+    const channel = interaction.channel; // Get the channel object from the interaction
+
+    const messages = await channel.messages.fetch({ limit: count });
+    const promises = messages.map(msg => msg.delete());
+    await Promise.all(promises);
+
+    const embed = new EmbedBuilder()
+      .setColor(0xee99ff)
+      .setTitle('削除完了！')
+      .setDescription(`削除したメッセージ数: ${promises.length}`);
+
+    await interaction.editReply({ content: '削除完了！' });
+    interaction.channel.send({ embeds: [embed] });
+  } catch (error) {
+    console.error('メッセージの削除中にエラーが発生しました:', error);
+    interaction.channel.send('メッセージの削除中にエラーが発生しました');
+  }
+},
 };
