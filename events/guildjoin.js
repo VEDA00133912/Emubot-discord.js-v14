c8a0fe816bb372237c91144db7c119d2d885ea19
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-30 21:17:09 +0900

    Update guildjoin.js

--- events/guildjoin.js
+++ events/guildjoin.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'guildCreate',
     execute(guild, client) {
-      const targetChannelId = "1236430386427334848"; // 通知を送信するチャンネルのID
+      const targetChannelId = "1242750255376633876"; // 通知を送信するチャンネルのID
       try {
         const targetChannel = client.channels.cache.get(targetChannelId);
         if (targetChannel) {
