43ad13ddcf3ac2fd038c1ab4973cf946e5e919d4
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-30 17:13:41 +0900

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,25 +1,29 @@
-// サーバー情報を表示するコマンド(今後項目を増やす)
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('serverinfo')
-    .setDescription('サーバー情報を表示します。'),
-  async execute(interaction) {
-    const guild = interaction.guild;
-    const embed = new EmbedBuilder()
-      .setColor(0xf8b4cb)
-      .setTitle('Server Info')
-      .addFields(
-        { name: 'サーバー名', value: guild.name, inline: true },
-        { name: 'メンバー数', value: guild.memberCount.toString(), inline: true },
-      );
-
-    try {
+    .setDescription('サーバー情報の表示'),
+    async execute(interaction) {
+      const guild = interaction.guild;
+      const serverIconUrl = guild.iconURL({ size: 1024 });
+      const textChannelsCount = guild.channels.cache.filter(c => c.type === 0).size;
+      const voiceChannelsCount = guild.channels.cache.filter(c => c.type === 2).size;
+      const categoryChannelsCount = guild.channels.cache.filter(c => c.type === 4).size;
+  
+      const embed = new EmbedBuilder()
+        .setColor(0xf8b4cb)
+        .setTimestamp()
+        .setThumbnail(serverIconUrl)
+        .addFields(
+          { name: "サーバーの名前", value: `${guild.name}` },
+          { name: "サーバーのID", value: `${guild.id}`, inline: true },
+          { name: "鯖主 👑", value: `<@${guild.ownerId}>` },
+          { name: "チャンネル数", value: `text 🗣: **${textChannelsCount}**\nvoice 🎤: **${voiceChannelsCount}**\ncategory: **${categoryChannelsCount}**`, inline: true },
+          { name: "メンバー数", value: `user 👪:**${guild.memberCount}**\nbot 🤖: **${guild.members.cache.filter(m => m.user.bot).size}**\nロール:**${guild.roles.cache.size}**`, inline: true }
+        );
+  
       await interaction.reply({ embeds: [embed] });
-    } catch (error) {
-      console.error(error);
-      console.log("serverinfo error");
     }
-  },
-};
+  };
