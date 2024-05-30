712916cf5eeaf17e9a9ef26c8e8c726f1d76bcf4
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-30 21:16:32 +0900

    Update convert.js

--- commands/convert.js
+++ commands/convert.js
@@ -19,36 +19,45 @@ module.exports = {
         .setDescription('変換するテキストを入力してください。')
         .setRequired(true)),
                      
-  async execute(interaction) {
-    const type = interaction.options.getString('type');
-    const text = interaction.options.getString('text');
-    const conversionData = require('../convert.json');
+async execute(interaction) {
+  const type = interaction.options.getString('type');
+  const text = interaction.options.getString('text');
+  const conversionData = require('../convert.json');
 
-    let convertedText = '';
-    switch (type) {
-      case 'rune':
-        convertedText = convertText(text, conversionData.rune);
-        break;
-      case 'phoenicia':
-        convertedText = convertText(text, conversionData.phoenicia);
-        break;
-      case 'hieroglyphs':
-        convertedText = convertText(text, conversionData.hieroglyphs);
-        break;
-      case 'reverse':
-        convertedText = text.split('').reverse().join('');
-        break;
-      case 'anagram':
-        convertedText = anagram(text);
-        break;
-      default:
-        await interaction.reply('その文字はサポートされていません。');
-        return;
-    }
+if (hasMention(text)) {
+  await interaction.reply({ content: 'メンションが含まれているため、変換を行いません。', ephemeral: true });
+  return;
+}
 
-    await interaction.reply(convertedText);
+  let convertedText = '';
+  switch (type) {
+    case 'rune':
+      convertedText = convertText(text, conversionData.rune);
+      break;
+    case 'phoenicia':
+      convertedText = convertText(text, conversionData.phoenicia);
+      break;
+    case 'hieroglyphs':
+      convertedText = convertText(text, conversionData.hieroglyphs);
+      break;
+    case 'reverse':
+      convertedText = text.split('').reverse().join('');
+      break;
+    case 'anagram':
+      convertedText = anagram(text);
+      break;
+    default:
+      await interaction.reply('その文字はサポートされていません。');
+      return;
+  }
+
+  await interaction.reply(convertedText);
   },
 };
+
+function hasMention(text) {
+  return /<@!?(\d+)>/.test(text);
+}
 
 function convertText(text, conversionMap) {
   return text.toUpperCase()
