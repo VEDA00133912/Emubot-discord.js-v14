5aab14d510b4ec744c69b81ddf6df89fd2252cc9
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-05-31 22:32:30 +0900

    Update announce create.js

--- commands/announce create.js
+++ commands/announce create.js
@@ -8,14 +8,12 @@ module.exports = {
         const guild = interaction.guild;
         const channelName = 'えむbot開発室';
 
-        // Check if the channel already exists
         const existingChannel = guild.channels.cache.find(channel => channel.name === channelName && channel.type === 0);
 
         if (existingChannel) {
             await interaction.reply('The channel "えむbot開発室" already exists.');
         } else {
             try {
-                // Create the channel
             const createdchannel = await guild.channels.create({
                 "name": "えむbot開発室",
                 "type": 0
@@ -23,7 +21,7 @@ module.exports = {
                 await interaction.reply('えむbot開発室を作りました');
             } catch (error) {
                 console.error('Error creating channel:', error);
-                await interaction.reply('エラーです。権限などを確認してください');
+            await interaction.editReply({ content: 'エラーです。権限などを確認してください', ephemeral: true });
             }
         }
     },
