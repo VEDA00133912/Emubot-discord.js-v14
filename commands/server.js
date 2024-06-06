68fa9f4d6debb361d9528bef75a4b8a518750b89
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-06 15:07:13 +0900

    Update server.js

--- commands/server.js
+++ commands/server.js
@@ -1,5 +1,6 @@
 // 参加しているサーバー一覧を表示するコマンド
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const cooldowns = new Map();
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,17 +8,26 @@ module.exports = {
     .setDescription('入ってる鯖一覧'),
   async execute(interaction) {
     const guilds = interaction.client.guilds.cache;
-      
+
     const embed = new EmbedBuilder()
-      .setColor(0xf8b4cb)
+      .setColor('#f8b4cb')
       .setTitle('サーバー一覧')
       .setDescription(guilds.map(guild => guild.name).join('\n'));
 
-    try {
-      await interaction.reply({ embeds: [embed] });
-    } catch (error) {
-      console.error(error);
-      console.log("server error");
+ try {
+  const reply = await interaction.reply({ embeds: [embed], ephemeral: true });
+} catch (error) {
+  console.error(error);
+  await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+}
+    if (!cooldowns.has(interaction.guildId)) {
+      cooldowns.set(interaction.guildId, new Set());
     }
+    const now = Date.now();
+    const expirationTime = now + 3600000;
+    cooldowns.get(interaction.guildId).add(interaction.user.id);
+    setTimeout(() => {
+      cooldowns.get(interaction.guildId).delete(interaction.user.id);
+    }, 3600000);
   },
 };
