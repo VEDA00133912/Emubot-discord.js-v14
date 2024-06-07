97a4ba056dec65be11abc985599a729a3b3f73a0
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-07 14:13:18 +0900

    Update sendmessage.js

--- events/sendmessage.js
+++ events/sendmessage.js
@@ -1,18 +1,24 @@
 module.exports = {
-    name: 'messageCreate',
-    execute(message, client) {
-      const targetChannelId = '1236430386427334848'; 
-      if (message.channel.id === targetChannelId) {
-        const targetMessage = message.content;
-        client.guilds.cache.forEach(guild => {
-          const targetChannel = guild.channels.cache.find(channel => channel.type === 0 && channel.name === 'えむbot開発室');
-          if (targetChannel) {
-            targetChannel.send(targetMessage)
-              .then(() => console.log("メッセージ転送したよ"))
-              .catch(error => console.error("メッセージの転送中にエラーでたよ:", error));
+  name: 'messageCreate',
+  execute(message, client) {
+    const targetChannelId = '1236430386427334848'; 
+    if (message.channel.id === targetChannelId) {
+      const targetMessage = message.content;
+      const sendPromises = [];
+      client.guilds.cache.forEach(guild => {
+        const targetChannel = guild.channels.cache.find(channel => channel.type === 0 && channel.name === 'えむbot開発室');
+        if (targetChannel) {
+          try {
+            sendPromises.push(targetChannel.send(targetMessage));
+          } catch (error) {
+            console.error("メッセージの転送中にエラーが発生しました:", error);
           }
-        });
-      }
+        }
+      });
+
+      Promise.all(sendPromises)
+        .then(() => console.log("メッセージを同時に転送しました"))
+        .catch(error => console.error("メッセージの転送中にエラーが発生しました:", error));
     }
-  };
-  
+  }
+};
