ca909b191e7f16b842dcb3dc370f6fad2e6d4b42
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-15 16:23:53 +0900

    Update messagelink.js

--- events/messagelink.js
+++ events/messagelink.js
@@ -16,18 +16,35 @@ module.exports = {
 
           try {
             const fetchedMessage = await client.guilds.cache.get(guildId)?.channels.cache.get(channelId)?.messages.fetch(messageId);
-  
+
             if (fetchedMessage) {
-              if (fetchedMessage.embeds.length > 0 || fetchedMessage.attachments.size > 0) {
-                console.log(`Embedまたは画像のため展開できませんでした: ${url}`);
+              const messageContent = fetchedMessage.content;
+              const hasEmbed = Boolean(fetchedMessage.embeds.length);
+              const hasAttachment = Boolean(fetchedMessage.attachments.size);
+
+              if (!messageContent && hasEmbed) {
+                console.log(`Error: Fetched message only contains an embed.`);
                 return;
               }
 
               const embed = new EmbedBuilder()
-                .setAuthor(fetchedMessage.author.tag, fetchedMessage.author.displayAvatarURL())
-                .setDescription(fetchedMessage.content)
                 .setColor(0xf8b4cb)
-                .setTimestamp(fetchedMessage.createdTimestamp);
+            .setTimestamp(fetchedMessage.createdTimestamp)
+                .setAuthor({ name: fetchedMessage.author.tag, iconURL: fetchedMessage.author.displayAvatarURL() });
+
+              if (hasAttachment) {
+                const attachment = fetchedMessage.attachments.first();
+                if (attachment.contentType.startsWith('image/')) {
+                  embed.setImage(attachment.proxyURL);
+                } else if (attachment.contentType.startsWith('video/')) {
+                  embed.addFields({ name: 'Video', value: `[動画ファイル](${attachment.proxyURL})` });
+                } else {
+                  embed.addFields({ name: 'File', value: `[${attachment.name}](${attachment.proxyURL})` });
+                }
+              }
+                  if (messageContent) {
+                    embed.setDescription(messageContent);
+                  }
 
               message.channel.send({ embeds: [embed] });
             }
