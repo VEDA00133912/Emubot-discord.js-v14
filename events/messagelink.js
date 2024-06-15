260685a892fd832b047f977343b3e6e661553234
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-15 15:38:19 +0900

    Update messagelink.js

--- events/messagelink.js
+++ events/messagelink.js
@@ -1,37 +1,41 @@
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    name: 'messageCreate',
-    async execute(message, client) {
-      const urlRegex = /(https?:\/\/[^\s]+)/g;
-      const urls = message.content.match(urlRegex);
+  name: 'messageCreate',
+  async execute(message, client) {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const urls = message.content.match(urlRegex);
+
+    if (urls) {
+      for (const url of urls) {
+        if (url.includes('discord.com/channels/')) {
+          const urlParts = url.split('/');
+          const guildId = urlParts[urlParts.indexOf('channels') + 1];
+          const channelId = urlParts[urlParts.indexOf('channels') + 2];
+          const messageId = urlParts[urlParts.indexOf('channels') + 3];
+
+          try {
+            const fetchedMessage = await client.guilds.cache.get(guildId)?.channels.cache.get(channelId)?.messages.fetch(messageId);
   
-      if (urls) {
-        for (const url of urls) {
-          if (url.includes('discord.com/channels/')) {
-            const urlParts = url.split('/');
-            const guildId = urlParts[urlParts.indexOf('channels') + 1];
-            const channelId = urlParts[urlParts.indexOf('channels') + 2];
-            const messageId = urlParts[urlParts.indexOf('channels') + 3];
-  
-            try {
-              const fetchedMessage = await client.guilds.cache.get(guildId)?.channels.cache.get(channelId)?.messages.fetch(messageId);
-  
-              if (fetchedMessage) {
-                const embed = new EmbedBuilder()
-                  .setAuthor({ name: fetchedMessage.author.tag, iconURL: fetchedMessage.author.displayAvatarURL() }) 
-                  .setDescription(fetchedMessage.content)
-                  .setColor(0xf8b4cb)
-                  .setTimestamp(fetchedMessage.createdTimestamp); 
-  
-                message.channel.send({ embeds: [embed] });
+            if (fetchedMessage) {
+              if (fetchedMessage.embeds.length > 0 || fetchedMessage.attachments.size > 0) {
+                console.log(`Embedまたは画像のため展開できませんでした: ${url}`);
+                return;
               }
-            } catch (error) {
-              console.error(error);
+
+              const embed = new EmbedBuilder()
+                .setAuthor(fetchedMessage.author.tag, fetchedMessage.author.displayAvatarURL())
+                .setDescription(fetchedMessage.content)
+                .setColor(0xf8b4cb)
+                .setTimestamp(fetchedMessage.createdTimestamp);
+
+              message.channel.send({ embeds: [embed] });
             }
+          } catch (error) {
+            console.error(error);
           }
         }
       }
     }
-  };
-  
+  }
+};
