1aba94ba79c0a9a14b52d4d7d9cc56b478a4958f
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-15 15:21:04 +0900

    Update channelcreate.js

--- events/channelcreate.js
+++ events/channelcreate.js
@@ -12,7 +12,20 @@ module.exports = {
             console.log(`チャンネルを作ったよ: ${channel.name} (${channel.id})`);
             await channel.send('botのお知らせ用チャンネルです。消さないでくれると嬉しいです(?)');
         } catch (error) {
-            console.error(`チャンネルを作れなかったよ: ${guild.name}:${guild.id}`, error);
+            // チャンネル作成権限がない場合の処理(調べたかんじ50013であってるはず..)
+            if (error.code === 50013) {
+                const owner = await guild.fetchOwner();
+  
+                try {
+                    await owner.send(`**えむbotのチャンネル作成権限がありません**\nチャンネルを作成するためには、えむbotにチャンネル作成権限を付与してください。`);
+                    console.log(`DMを送信したよ: ${owner.user.tag}`);
+                } catch (error) {
+                    console.error(`DMを送信できなかったよ: ${owner.user.tag}`, error);
+                }
+            } else {
+                // その他のエラー
+                console.error(`チャンネルを作れなかったよ: ${guild.name}:${guild.id}`, error);
+            }
         }
     }
 }
