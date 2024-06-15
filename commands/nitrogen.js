5e3661195034e9b6fefb7b15c8225872d2e76f9d
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-15 15:14:04 +0900

    Update nitrogen.js

--- commands/nitrogen.js
+++ commands/nitrogen.js
@@ -1,8 +1,8 @@
-const { SlashCommandBuilder,EmbedBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('nitrogen')
+        .setName('fake-nitrogen')
         .setDescription('nitroギフトリンクの生成')
         .addIntegerOption(option =>
             option.setName('count')
@@ -14,17 +14,21 @@ module.exports = {
             await interaction.deferReply(); 
 
             const quantity = interaction.options.getInteger('count');
-            const nitroLinks = generateNitroLinks(quantity);
+
+            const countLimited = Math.min(quantity, 10);
+            if (quantity > 10) {
+                return interaction.editReply({ content: '生成する数は10以下にしてね' });
+            }
+            const nitroLinks = generateNitroLinks(countLimited);
 
             const embed = new EmbedBuilder()
                 .setColor('#f47fff')
-                .setTitle('Nitro Gift Links')
-                .setDescription(`Nitroギフトリンク\n${nitroLinks.join('\n')}`);
-            
-            await interaction.followUp({ embeds: [embed] });
+                .setDescription(`**Fake Nitro Gift Links** <a:nitronitronitro:1240815301801545789>\nNitroギフトリンク\n${nitroLinks.join('\n')}`);
+
+            await interaction.editReply({ embeds: [embed] });
         } catch (error) {
             console.error('Nitro ギフトリンクの生成中にエラーが発生しました：', error);
-            await interaction.followUp({ content: 'Nitro ギフトリンクの生成中にエラーが発生しました。', ephemeral: true });
+            await interaction.editReply({ content: 'Nitro ギフトリンクの生成中にエラーが発生しました。', ephemeral: true });
         }
     },
 };
@@ -32,7 +36,7 @@ module.exports = {
 function generateNitroLinks(quantity) {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const nitroLinks = [];
-    
+
     for (let j = 0; j < quantity; j++) {
         let code = '';
         for (let i = 0; i < 16; i++) {
