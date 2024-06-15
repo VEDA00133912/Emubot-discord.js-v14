ae47832484c56c811ce1053bcbd680af4e592a3b
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-15 10:47:43 +0900

    Update announce create.js

--- commands/announce create.js
+++ commands/announce create.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -8,20 +8,28 @@ module.exports = {
         const guild = interaction.guild;
         const channelName = 'えむbot開発室';
 
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return interaction.reply({ content: 'あなたに実行権限が有りません。', ephemeral: true });
+        }
+
+        if (!interaction.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+            return interaction.reply({ content: 'チャンネルの作成権限がありません。', ephemeral: true });
+        }
+
         const existingChannel = guild.channels.cache.find(channel => channel.name === channelName && channel.type === 0);
 
         if (existingChannel) {
-            await interaction.reply('The channel "えむbot開発室" already exists.');
+            await interaction.reply({ content: 'The channel "えむbot開発室" already exists.', ephemeral: true });
         } else {
             try {
-            const createdchannel = await guild.channels.create({
-                "name": "えむbot開発室",
-                "type": 0
-            });
+                const createdChannel = await guild.channels.create({
+                    name: channelName,
+                    type: 0
+                });
                 await interaction.reply('えむbot開発室を作りました');
             } catch (error) {
                 console.error('Error creating channel:', error);
-            await interaction.editReply({ content: 'エラーです。権限などを確認してください', ephemeral: true });
+                await interaction.reply({ content: 'エラーが発生しました', ephemeral: true });
             }
         }
     },
