2de1c26b02437464bc04d842bce8934e25922f39
りょう <143770907+VEDA00133912@users.noreply.github.com>
2024-06-17 21:46:41 +0900

    Update verifypanel.js

--- commands/verifypanel.js
+++ commands/verifypanel.js
@@ -34,24 +34,4 @@ module.exports = {
 
         await interaction.reply({ embeds: [embed], components: [row]});
     },
-    async handleButtonInteraction(interaction) {
-        const roleId = interaction.customId;
-        const role = interaction.guild.roles.cache.get(roleId);
-
-        if (!role) {
-            return interaction.reply({ content: 'ロールが見つかりません。', ephemeral: true });
-        }
-
-        if (interaction.guild.members.me.roles.highest.comparePositionTo(role) <= 0) {
-            return interaction.reply({ content: 'botロールより上のロールは付与できません。', ephemeral: true });
-        }
-
-        try {
-            await interaction.member.roles.add(role);
-            return interaction.reply({ content: `ロール ${role.name} が付与されました。<a:1196945710009036821:1247370449604841492>`, ephemeral: true });
-        } catch (error) {
-            console.error(error);
-            return interaction.reply({ content: 'ロールの付与に失敗しました。', ephemeral: true });
-        }
-    },
 };
